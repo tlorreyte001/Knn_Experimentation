0603de54f979d017ed46693a4b7294011345c2a3
Thomas Lorreyte <tom.lorreyte@gmail.com>
2019-03-26 00:43:42 +0000

    45% d'erreur à la reconnaissance, pas mal ptdr

--- eigenfaces.m
+++ eigenfaces.m
@@ -30,18 +30,17 @@ Nc = length(cls_trn);
 % Number of training images in each class
 size_cls_trn = [bd(2:Nc)-bd(1:Nc-1);N-bd(Nc)+1]; 
 % Display the database
-F = zeros(192*Nc,168*max(size_cls_trn));
-for i=1:Nc
-   for j=1:size_cls_trn(i)
-         pos = sum(size_cls_trn(1:i-1))+j;
-         F(192*(i-1)+1:192*i,168*(j-1)+1:168*j) = reshape(data_trn(:,pos),[192,168]);
-   end
-end
-figure;
-imagesc(F);
-colormap(gray);
-axis off;
-
+% F = zeros(192*Nc,168*max(size_cls_trn));
+% for i=1:Nc
+%    for j=1:size_cls_trn(i)
+%          pos = sum(size_cls_trn(1:i-1))+j;
+%          F(192*(i-1)+1:192*i,168*(j-1)+1:168*j) = reshape(data_trn(:,pos),[192,168]);
+%    end
+% end
+% figure;
+% imagesc(F);
+% colormap(gray);
+% axis off;
 
 %% ACP
 %Question 1 : Calcul des vecteurs propres
@@ -55,6 +54,7 @@ end
 U=X*V*(V'*X'*X*V)^-(1/2);
 
 %Question 2 :
+
 % figure;
 % for i=1:N
 %     F=reshape(U(:,i),[192,168]);
@@ -64,11 +64,10 @@ U=X*V*(V'*X'*X*V)^-(1/2);
 % end
 % colormap(gray);
 
-
-
 %Question 3 : 
+
 % figure;
-personne=19;
+% personne=19;
 % Xpro=zeros(P,N);
 % for l=1:N-1
 %     subplot(6,10,l)
@@ -84,6 +83,7 @@ personne=19;
 % end
 
 %Question 4 : 
+
 % figure;
 ratio=[];
 ratio(1)=lambda(N,N)/sum(diag(lambda));
@@ -100,53 +100,48 @@ end
 %% CLASSIFICATION
 
 %Question 1
-
-
-k=7;
-Nu=[];
+%Omega des img de training
+W=[];
 for i=1:N
-    Nu(i)=norm(omega(X,U,personne,l, N)-omega(X,U,i,l,N));
-end
-
-
-Numin_indice=zeros(1,k);
-[Nusort,I]=sort(Nu);
-for i=1:k
-    Numin_indice(i)=I(i);
-end
-
-phi=[];
-for j=1:6
-    a=size(intersect(Numin_indice,(size_cls_trn(1)*(j-1)+1:size_cls_trn(1)*j)));
-    phi(j)=a(2);
-end
-
-[scorekNN, kNN]=max(phi);
-
-%% Matrice de confusion
-
-% Data extraction
-% Test set
-adr = './database/test1/';
-fld = dir(adr);
-nb_elt_test = length(fld);
-% Data matrix containing the testing images in its columns 
-data_test = []; 
-% Vector containing the class of each testing image
-lb_test = []; 
-for i=1:nb_elt_test
-    if fld(i).isdir == false
-        lb_test = [lb_test ; str2num(fld(i).name(6:7))];
-        img = double(imread([adr fld(i).name]));
-        data_test = [data_test img(:)];
+    wtmp=[];
+    for j=1:l
+        wtmp=[wtmp, X(:,i)'*U(:,N-j+1)];
     end
+    W=[W;wtmp];
 end
-% Size of the testing set
-[P_test,size_test] = size(data_test);
-% Classes contained in the testing set
-[~,I]=sort(lb_test);
-data_test = data_test(:,I);
-[cls_test,bd,~] = unique(lb_test);
-Nc = length(cls_test); 
-% Number of training images in each class
-size_cls_test = [bd(2:Nc)-bd(1:Nc-1);N-bd(Nc)+1];
+
+k=6;
+% lb_test=k_NN(k, './database/test1/', W, l, U, N)
+% module_Cj=7
+% lb=[ones(1,7), 2*ones(1,7) ,3*ones(1,7) ,4*ones(1,7),5*ones(1,7), 6*ones(1,7)];
+% [C,err_rate]=confmat(lb',lb_test');
+% C
+
+%% Classification Gaussienne
+
+%Calcul des moyennes intra-classe à partir des données d'apprentissage
+MU=[];
+for i=1:6
+    MU=[MU; 1/10*sum(W((i-1)*10+1:i*10,:))];
+    
+end
+
+%Calcul de la covariance
+SIGMA=0;
+for i=1:6
+    for j=1:10
+        a= W(j+(10*(i-1)),:)-MU(i,:);
+        SIGMA=SIGMA+(a*a');
+    end    
+end
+SIGMA=(1/N)*SIGMA; %Bizarre
+
+%Question 1: %TODO
+omega=[];
+for i=1:30
+    fiy=sqrt(1/2*pi*det(SIGMA))*exp(-1/2*(data_trn(
+    omega[
+    
+
+
+       
--- k_NN.m
+++ k_NN.m
@@ -1,26 +1,47 @@
-function [ kNN ] = k_NN( k, data, X, U, image, l, N, size_cls_test)
+function [ Sortie ] = k_NN( k, adr, W, l, U, N )
 %K_NN Summary of this function goes here
 %   Detailed explanation goes here
-Nu=[];
-for i=1:N
-    Nu(i)=norm(omega(data, U, image, l, N) - omega(X, U, i, l, N));
+
+Sortie=[];
+
+%Extraction des données de test
+fld = dir(adr);
+nb_elt_test = length(fld);
+for i=1:nb_elt_test
+    if fld(i).isdir == false
+        img = double(imread([adr fld(i).name]));
+        data_test = img(:);
+        
+        %Calcul d'omega pour l'image test
+        omega=[];
+        for j=1:l
+            omega=[omega, data_test.'*U(:,N-j+1)];
+        end
+        
+        %Calcul de Nu
+        Nu=[];
+        for j=1:N
+            Nu=[Nu, norm(omega-W(j,:))];
+        end
+        
+        Numin_indice=[];
+        [Nusort,I]=sort(Nu);
+        for j=1:k
+            Numin_indice=[Numin_indice, I(j)];
+        end
+        
+        %Calcul de phi
+        phi=[];
+        for j=1:6
+            a=size(intersect(Numin_indice,(((j-1)*10+1:j*10))));
+            phi(j)=a(2);
+        end
+
+  
+        [scorekNN, kNN]=max(phi);
+        
+        Sortie=[Sortie kNN];
+    end
 end
 
-
-Numin_indice=zeros(1,k);
-[Nusort,I]=sort(Nu);
-for i=1:k
-    Numin_indice(i)=I(i);
-end
-
-phi=[];
-for j=1:6
-    a=size(intersect(Numin_indice,(size_cls_test*(j-1)+1:size_cls_test*j)));
-    phi(j)=a(2);
-end
-
-[scorekNN, kNN]=max(phi);
-
-
-end
 
